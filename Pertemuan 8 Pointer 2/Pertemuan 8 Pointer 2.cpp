93300faa6db0427eff08057e95ddef97848184ad
gibranrafii <gibranrafi21@gmail.com>
2024-05-07 10:59:14 +0700

    Membuat class mahasiswa dengan variabel nim yang bersifat public serta membuat prosedur shownim

---
+++ Pertemuan 8 Pointer 2/Pertemuan 8 Pointer 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+   
+}
